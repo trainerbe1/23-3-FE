f4f4188080b74b8064de16de81bf55d3753a6d33
Melvin-Jovano <melvinjovano2@gmail.com>
2024-06-03 09:24:23 +0700

    feat: improvise login

--- src/views/AuthView.jsx
+++ src/views/AuthView.jsx
@@ -1,5 +1,6 @@
 import { toast } from "react-toastify";
 import Logo from "../assets/png/lunch.png";
+import BgLogin from "../assets/jpg/bg-login.jpg";
 import app from "../common/app";
 import theme from "../common/theme";
 import { useNavigate } from "react-router-dom";
@@ -16,18 +17,20 @@ function AuthView() {
 
   return (
     <div className='h-screen flex items-center justify-center'>
-      <div className="dark:bg-gray-800 bg-white rounded p-5">
+      <img className="brightness-50 absolute h-screen w-screen z-0" src={BgLogin} alt="" srcSet="" />
+      <div className="absolute h-screen w-screen z-0 bg-gray-800 opacity-50"></div>
+
+      <div className="dark:bg-gray-800 bg-white rounded p-5 z-10">
         <div className="mb-10 flex items-center">
           <div>
             <img src={Logo} className="h-8 me-3" alt="Logo"/> 
           </div>
-          <div className="text-white text-lg font-semibold">
+          <div className="text-white text-2xl font-bold">
             {app.name}
           </div>
         </div>
-
         <div>
-          <div className="text-lg font-bold mb-4 text-white">Login to continue</div>
+          <div className="text-2xl font-bold mb-6 text-white">Login to continue</div>
           <form onSubmit={onSubmit}>
             <input required type="text" className={`mb-5 ${theme.textfield}`} placeholder="Username" />
             <input required type="password" className={`mb-8 ${theme.textfield}`} placeholder="Password" />
@@ -35,6 +38,7 @@ function AuthView() {
           </form>
         </div>
       </div>
+
     </div>
   );
 }
